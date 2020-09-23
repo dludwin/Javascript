d9c32162181b34fbbbe47b2a6bbdb950a0ff5ddc
dludwin <48782906+dludwin@users.noreply.github.com>
2020-09-23 09:34:57 +0200

    a

--- Edabit/every.js
+++ Edabit/every.js
@@ -9,4 +9,4 @@ console.log(checkAllEven([2, 4, 6]));
 /* every() returns a boolean - true if every element in this array 
 satisfies the provided testing function. An important difference
  with . ... forEach() returns nothing - It iterates the
- Array performing a given action for each item in the Array. */
+ Array performing a given action for each item in the Array.s */
